8f4bc85d295e6fbac3b4c8f9532ec972c7f69550
lilliputten <lilliputten@gmail.com>
2025-01-27 10:10:07 +0300

    Issue #33: Added reading apk build information (from json), display of the actual link in the `right-mobile-promo` section.

--- core/appConfig.py
+++ core/appConfig.py
@@ -1,8 +1,33 @@
 # -*- coding:utf-8 -*-
 
+import os.path
+import json
+import posixpath
+
 from .appEnv import (
+    BASE_DIR,
     appEnv,
 )
 
 # Timezone (set `TZ_HOURS` to hours value to adjust date representation to corresponding timezone)
 TZ_HOURS = appEnv.int('TZ_HOURS_OFFSET', 0)
+
+APK_FOLDER = appEnv.str('APK_FOLDER', 'static/downloads/apk')
+APK_JSON_FILE = appEnv.str('APK_JSON_FILE', posixpath.join(APK_FOLDER, 'output-metadata.json'))
+APK_DOWNLOAD_FILE = ''
+APK_DOWNLOAD_VERSION = ''
+APK_DOWNLOAD_SIZE = 0
+
+try:
+    # if os.path.isfile(APK_JSON_FILE):
+    #     pass
+    filepath = posixpath.join(BASE_DIR, APK_JSON_FILE)
+    with open(filepath) as f:
+        data = json.load(f)
+        element = data['elements'][0]
+        APK_DOWNLOAD_FILE = posixpath.join(APK_FOLDER, element['outputFile'])
+        apkPath = posixpath.join(BASE_DIR, APK_DOWNLOAD_FILE)
+        APK_DOWNLOAD_SIZE = os.stat(apkPath).st_size
+        APK_DOWNLOAD_VERSION = element['versionName']
+except Exception as err:
+    print('Can get APK_DOWNLOAD_FILE from APK_JSON_FILE:', repr(err))
