17cc7a5655de5031c94d29dcb96c06c5376416b5
lilliputten <lilliputten@gmail.com>
2025-03-23 05:45:02 +0300

    Issue #64: Attempt to optimize processing of track image previews.

--- tales_django/entities/Tracks/constants/preview_picture_sizes.py
+++ tales_django/entities/Tracks/constants/preview_picture_sizes.py
@@ -1,4 +1,4 @@
-track_preview_picture_full_size = 700
+track_preview_picture_full_size = 600
 track_preview_picture_thumb_size = round(track_preview_picture_full_size / 10)
 track_preview_picture_jpeg_quality = 70
 
