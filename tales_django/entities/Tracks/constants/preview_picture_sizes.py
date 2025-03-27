2742f04009f58465f0ee86e871c6885f43140111
lilliputten <lilliputten@gmail.com>
2025-03-28 01:30:28 +0300

    Minor changes. Updated lqip thumbnail.

--- tales_django/entities/Tracks/constants/preview_picture_sizes.py
+++ tales_django/entities/Tracks/constants/preview_picture_sizes.py
@@ -1,11 +1,17 @@
+default_preview_jpeg_quality = 70
+
+micro_image_thumb_size = 8
+small_image_thumb_size = 32
+
+track_small_image_thumb_size = small_image_thumb_size
+
 track_preview_picture_full_size = 600
 track_preview_picture_thumb_size = round(track_preview_picture_full_size / 10)
-track_small_image_thumb_size = 40
-track_preview_picture_jpeg_quality = 70
+track_preview_picture_jpeg_quality = default_preview_jpeg_quality
 
 author_portrait_picture_full_size = 150
 author_portrait_picture_thumb_size = round(author_portrait_picture_full_size / 10)
-author_portrait_picture_jpeg_quality = 70
+author_portrait_picture_jpeg_quality = default_preview_jpeg_quality
 
 __all__ = [
     'track_preview_picture_full_size',
--- tales_django/imagegenerators.py
+++ tales_django/imagegenerators.py
@@ -4,10 +4,23 @@ from imagekit.processors import (
 )
 
 from tales_django.entities.Tracks.constants.preview_picture_sizes import (
-    track_preview_picture_jpeg_quality,
-    # track_preview_picture_thumb_size,
+    default_preview_jpeg_quality,
     track_small_image_thumb_size,
+    micro_image_thumb_size,
 )
+
+
+class micro_image_thumb(ImageSpec):
+    processors = (
+        [
+            ResizeToFit(micro_image_thumb_size, micro_image_thumb_size),
+        ],
+    )
+    format = 'JPEG'
+    options = {'quality': default_preview_jpeg_quality}
+
+
+register.generator('tales_django:micro_image_thumb', micro_image_thumb)
 
 
 class small_image_thumb(ImageSpec):
@@ -17,7 +30,7 @@ class small_image_thumb(ImageSpec):
         ],
     )
     format = 'JPEG'
-    options = {'quality': track_preview_picture_jpeg_quality}
+    options = {'quality': default_preview_jpeg_quality}
 
 
 register.generator('tales_django:small_image_thumb', small_image_thumb)
