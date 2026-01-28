af370b421e7053f30da6463ae169e7f8b29aa024
lilliputten <lilliputten@gmail.com>
2026-01-28 17:31:06 +0300

    Issue #91: Added a form for managing the series tracks on the SeriesAdmin form.

--- tales_django/entities/Tracks/admin/SeriesAdmin.py
+++ tales_django/entities/Tracks/admin/SeriesAdmin.py
@@ -1,6 +1,12 @@
-from django.contrib import admin
+from django import forms
+from django.contrib import admin, messages
 from django.db.models import Count, F
 from django.db.models.functions import Lower
+from django.http import HttpResponseRedirect
+from django.shortcuts import redirect
+from django.template.response import TemplateResponse
+from django.urls import path, reverse
+from django.utils.html import format_html
 from django.utils.translation import get_language
 from django.utils.translation import gettext_lazy as _
 from import_export.admin import ExportActionModelAdmin, ImportExportModelAdmin
@@ -11,7 +17,7 @@ from unfold.contrib.import_export.forms import ExportForm, ImportForm
 from tales_django.sites import unfold_admin_site
 
 from ..forms.TrackInlineForm import TrackInlineForm
-from ..models import Series
+from ..models import Series, Track
 
 
 @admin.action(description=_('Promote'))
@@ -24,11 +30,71 @@ def no_promote_action(modeladmin, request, queryset):
     queryset.update(promote=False)
 
 
+class TrackAssignmentForm(forms.Form):
+    """
+    Form to select an existing track to assign to a series.
+    """
+
+    select_track = forms.ModelChoiceField(
+        queryset=Track.objects.none(),
+        required=False,
+        empty_label=_('Select a track to add'),
+        label=_('Select track to add to this series'),
+        widget=forms.Select(attrs={'class': 'select2'}),
+    )
+
+    def __init__(self, *args, **kwargs):
+        series_instance = kwargs.pop('series_instance', None)
+        super().__init__(*args, **kwargs)
+
+        if series_instance:
+            # Get tracks that are not already in this series
+            tracks_already_in_series = series_instance.tracks.values_list('id', flat=True)
+            available_tracks = Track.objects.exclude(id__in=tracks_already_in_series)
+            self.fields['select_track'].queryset = available_tracks
+
+
 @admin.register(Series, site=unfold_admin_site)
 class SeriesAdmin(TranslatedFieldAdmin, ImportExportModelAdmin, ExportActionModelAdmin, UnfoldModelAdmin):
     inlines = [TrackInlineForm]
     import_form_class = ImportForm
     export_form_class = ExportForm
+    change_form_template = 'admin/entities/Tracks/series/change_form.html'
+
+    def get_urls(self):
+        urls = super().get_urls()
+        # No custom URLs for now to avoid the NoReverseMatch error
+        return urls
+
+    def change_view(self, request, object_id, form_url='', extra_context=None):
+        extra_context = extra_context or {}
+        series = self.get_object(request, object_id)
+
+        if request.method == 'POST' and 'select_track' in request.POST:
+            # Handle track assignment form submission
+            track_assignment_form = TrackAssignmentForm(request.POST, series_instance=series)
+            if track_assignment_form.is_valid():
+                selected_track = track_assignment_form.cleaned_data['select_track']
+                if selected_track:
+                    selected_track.series = series
+                    selected_track.series_order = series.tracks.count() + 1
+                    selected_track.save()
+                    messages.success(
+                        request, _('Track "%(track)s" has been added to the series.') % {'track': selected_track.title}
+                    )
+                # Redirect to refresh the page after assignment
+                return HttpResponseRedirect(request.path)
+        else:
+            # Display the form for GET requests or after successful assignment
+            track_assignment_form = TrackAssignmentForm(series_instance=series)
+
+        if series:
+            # Add form to select existing tracks to assign
+            extra_context['track_assignment_form'] = track_assignment_form
+            # For now, we'll handle the assignment via a POST to the same page
+            extra_context['assign_tracks_url'] = request.path
+
+        return super().change_view(request, object_id, form_url, extra_context)
 
     actions = [
         promote_action,
--- tales_django/entities/Tracks/forms/TrackInlineForm.py
+++ tales_django/entities/Tracks/forms/TrackInlineForm.py
@@ -1,7 +1,87 @@
+from django import forms
 from django.contrib import admin
+from django.forms.models import BaseInlineFormSet
 from django.utils.translation import gettext_lazy as _
 
-from ..models import Track
+from ..models import Series, Track
+
+
+class TrackAssignmentForm(forms.Form):
+    """
+    Form to select existing tracks to assign to a series.
+    """
+
+    tracks_to_add = forms.ModelMultipleChoiceField(
+        queryset=Track.objects.none(),  # Will be populated dynamically
+        required=False,
+        widget=forms.CheckboxSelectMultiple,
+        label=_('Select tracks to add to this series'),
+    )
+
+    def __init__(self, *args, **kwargs):
+        series_instance = kwargs.pop('series_instance', None)
+        super().__init__(*args, **kwargs)
+
+        if series_instance:
+            # Get tracks that are not already in this series
+            tracks_already_in_series = series_instance.tracks.values_list('id', flat=True)
+            available_tracks = Track.objects.exclude(id__in=tracks_already_in_series)
+            self.fields['tracks_to_add'].queryset = available_tracks
+
+
+class TrackSelectorWidget(forms.Select):
+    """
+    Custom widget to select existing tracks that are not already in the series.
+    """
+
+    def __init__(self, series_instance=None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.series_instance = series_instance
+
+    def build_attrs(self, base_attrs, extra_attrs=None):
+        attrs = super().build_attrs(base_attrs, extra_attrs)
+        # Add data attributes that can be used by JavaScript
+        if self.series_instance:
+            attrs['data-series-id'] = self.series_instance.id
+        attrs['class'] = attrs.get('class', '') + ' track-selector-dropdown'
+        return attrs
+
+
+class TrackSelectorForm(forms.Form):
+    """
+    Form with a dropdown to select existing tracks to add to the series.
+    """
+
+    select_track = forms.ModelChoiceField(
+        queryset=Track.objects.none(),
+        required=False,
+        empty_label=_('Select a track to add'),
+        label=_('Add existing track to series'),
+        widget=TrackSelectorWidget(),
+    )
+
+    def __init__(self, *args, **kwargs):
+        series_instance = kwargs.pop('series_instance', None)
+        super().__init__(*args, **kwargs)
+
+        if series_instance:
+            # Get tracks that are not already in this series
+            tracks_already_in_series = series_instance.tracks.values_list('id', flat=True)
+            available_tracks = Track.objects.exclude(id__in=tracks_already_in_series)
+            self.fields['select_track'].queryset = available_tracks
+            # Update the widget with the series instance
+            self.fields['select_track'].widget.series_instance = series_instance
+
+
+class TrackSelectionForm(forms.Form):
+    """Selection-only form for existing tracks"""
+
+    track_id = forms.ModelChoiceField(
+        queryset=Track.objects.none(),
+        label=_('Select track to add'),
+        empty_label=_('Choose a track...'),
+        widget=forms.Select(attrs={'class': 'select2'}),
+    )
 
 
 class TrackInlineForm(admin.TabularInline):
@@ -11,7 +91,11 @@ class TrackInlineForm(admin.TabularInline):
 
     model = Track
     fk_name = 'series'  # Specify which foreign key to use
-    extra = 1  # Number of empty forms to display
+    extra = 0  # No empty forms to prevent creating new tracks directly
+    can_add = False  # Disable the add button
     fields = ('title_ru', 'title_en', 'series_order')
     verbose_name = _('Track')
     verbose_name_plural = _('Tracks')
+    can_delete = True  # Allow removal of existing tracks from the series
+    show_change_link = False  # Don't show change link since we're not editing data
+    readonly_fields = ('title_ru', 'title_en', 'series_order')  # Make fields read-only to prevent editing
